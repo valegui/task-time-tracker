c5cf51074a724b81c05db9f00be8b604188aa662
Valeria Guidotti <valeriaguidotti@gmail.com>
2024-05-20 19:43:23 -0400

    register and open view in a right leaf

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, WorkspaceLeaf } from 'obsidian';
 import { TaskTimeTrackerSettings, DEFAULT_SETTINGS } from './settings';
 import { TaskTimeTrackerSettingTab } from './settings-tab';
-
+import { TASK_TIME_TRACKER_VIEW, TaskTimeTrackerView } from './view';
 export default class TaskTimeTrackerPlugin extends Plugin {
 	settings: TaskTimeTrackerSettings;
 
@@ -9,23 +9,39 @@ export default class TaskTimeTrackerPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('square-gantt-chart', 'Task Time Tracker', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.addRibbonIcon('clipboard-list', 'Show Task Time Tracker', () => {
+			this.openView();
 		});
 
+		// This adds view for contents of the plugin
+		this.registerView(TASK_TIME_TRACKER_VIEW, (leaf) => new TaskTimeTrackerView(leaf))
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new TaskTimeTrackerSettingTab(this.app, this));
 
+		// Command palette
 		this.addCommand({
 			id: 'start-empty-task',
 			name: 'Start Empty Task',
+			callback() {
+
+			}
+		})
+
+		this.addCommand({
+			id: 'start-task',
+			name: 'Start Task',
+			callback() {
+
+			}
+		})
+
+		this.addCommand({
+			id: 'end-current-task',
+			name: 'End Current Task',
 			checkCallback: (checking: boolean) => {
 				return false;
 			}
-		}
-		)
-
+		})
 	}
 
 
@@ -39,6 +55,19 @@ export default class TaskTimeTrackerPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	openView() {
+		this.app.workspace.detachLeavesOfType(TASK_TIME_TRACKER_VIEW);
+
+		let leaf: WorkspaceLeaf | null = null;
+		leaf = this.app.workspace.getRightLeaf(false);
+		leaf!.setViewState({
+			type: TASK_TIME_TRACKER_VIEW,
+
+		});
+
+		this.app.workspace.revealLeaf(leaf!);
 	}
 }
 
