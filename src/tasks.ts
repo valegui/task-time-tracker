5a677a4dfac0df61d9ac6fbf341c82b481177b3e
Valeria Guidotti <valeriaguidotti@gmail.com>
2024-05-20 01:43:48 -0400

    add function to create new task

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,3 +1,5 @@
+import { start } from "repl";
+
 export interface TaskTracker {
     tasks: Task[];
     currentTask?: Task;
@@ -6,8 +8,8 @@ export interface TaskTracker {
 export interface Task {
     name: string;
     startTime: string;
-    endTime: string;
-    duration: string;
+    endTime: string | null;
+    duration: string | null;
     category?: string;
     project?: string;
 }
@@ -15,4 +17,19 @@ export interface Task {
 function isRunning(tracker: TaskTracker): boolean {
     if (typeof tracker.currentTask == 'undefined') return false;
     return true;
+}
+
+function newTask(name?: string, category?: string, project?: string): Task {
+    if (typeof name == 'undefined') {
+        name = Math.random().toString(30).slice(2, 20);
+    }
+    let startTime = Date.now().toString();
+    return {
+        name: name,
+        startTime: startTime,
+        endTime: null,
+        duration: null,
+        category,
+        project
+    };
 }
