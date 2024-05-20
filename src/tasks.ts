6b3cb2f048c531e3688193a4c73e2af9730149a5
Valeria Guidotti <valeriaguidotti@gmail.com>
2024-05-20 00:00:07 -0400

    add optional fields to tracker and task

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,12 +1,13 @@
 export interface TaskTracker {
     tasks: Task[];
+    currentTask?: Task;
 }
 
 export interface Task {
     name: string;
-    category: string;
-    project: string;
     startTime: string;
     endTime: string;
     duration: string;
+    category?: string;
+    project?: string;
 }
