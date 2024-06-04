1dc43f0b4b656c3b4f8e3675328bf6d1cc28698c
Valeria Guidotti <valeriaguidotti@gmail.com>
2024-06-03 22:27:53 -0400

    feat: add file to use for tracker

--- src/settings.ts
+++ src/settings.ts
@@ -1,10 +1,12 @@
 export interface TaskTimeTrackerSettings {
+	trackerFile: string;
 	weekStart: string;
 	category: boolean;
 	project: boolean;
 }
 
 export const DEFAULT_SETTINGS: TaskTimeTrackerSettings = {
+	trackerFile: 'Tracker.md',
 	weekStart: 'Monday',
 	category: true,
 	project: true,
