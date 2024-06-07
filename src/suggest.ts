0a5f3c876fd6a7415d5d9da6e04f53220c67d0df
Valeria Guidotti <valeriaguidotti@gmail.com>
2024-06-07 00:04:55 -0400

    refactor: implement file suggest as abstractiputsuggester

--- src/suggest.ts
+++ src/suggest.ts
@@ -1,12 +1,31 @@
-import { TFile, type ISuggestOwner} from "obsidian";
+import { TFile, AbstractInputSuggest, App} from "obsidian";
 
-export class FileSuggest implements ISuggestOwner<string>{
-    renderSuggestion(value: string, el: HTMLElement): void {
-        throw new Error("Method not implemented.");
+export class FileSuggest extends AbstractInputSuggest<string> {
+    content: Set<TFile>;
+
+    constructor(private inputEl: HTMLInputElement, content: Set<TFile>, private onSelectCb: (value: string) => void, app: App) {
+        super(app, inputEl);
+        this.content = content;
     }
-    selectSuggestion(value: string, evt: MouseEvent | KeyboardEvent): void {
-        throw new Error("Method not implemented.");
+
+    getSuggestions(input: string): string[] {
+        const lowerCaseInput = input.toLocaleLowerCase();
+        let files = [...this.content].filter((content) =>
+            content.path.contains(lowerCaseInput)
+        );
+        return files.map((tfiles) =>
+            tfiles.path
+        );
     }
 
+    renderSuggestion(content: string, el: HTMLElement): void {
+        el.setText(content);
+    }
 
+    selectSuggestion(content: string, evt: MouseEvent | KeyboardEvent): void {
+        this.onSelectCb(content);
+        this.inputEl.value = "";
+        this.inputEl.blur();
+        this.close();
+    }
 }
