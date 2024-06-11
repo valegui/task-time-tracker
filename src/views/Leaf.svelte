3655bfa55232129ce874a1b38000716457abc85e
Valeria Guidotti <valeriaguidotti@gmail.com>
2024-06-10 22:13:16 -0400

    feat; change input form in side leaf

--- src/views/Leaf.svelte
+++ src/views/Leaf.svelte
@@ -10,24 +10,67 @@
   <hr class="divider">
 
   <div style="margin-left:5px;">
-    <h3 class="title">Time</h3>
-    <dl class="text" style="margin: 0 20px 20px 5px;">
-      <dt> Add manual time </dt>
-      <dt> Start timer </dt>
-      <dt> Stop timer </dt>
-    </dl>
-    
-    <h3 class="title">Categories</h3>
-    <dl class="text" style="margin: 0 20px 20px 5px;">
-      <dt> See current categories </dt>
-      <dt> Add category </dt>
-    </dl>
+    <div class="text">
+    <h3 class="title">Add manual time</h3>
+      <form action="new-manual-time" method="POST">
+        <div class="form-horizontal">
+          <label for="taskname" class="form-label"> Task name </label>
+          <input
+            type="text"
+            name="taskname"
+            id="taskname"
+            class="form-input"
+            placeholder="Task name"
+          />
+        </div>
+        <div class="form-horizontal">
+          <label for="starttime" class="form-label">Start Time</label>
+          <input
+            type="datetime-local"
+            name="starttime"
+            class="form-input"
+            id="starttime"
+          />
+        </div>
+        <div class="form-horizontal">
+          <label for="endtime" class="form-label">End Time</label>
+          <input
+            type="datetime-local"
+            name="endtime"
+            class="form-input"
+            id="endtime"
+          />
+        </div>
+        <div class="form-horizontal">
+          <label for="categoryname" class="form-label"> Category </label>
+          <input
+            type="text"
+            name="categoryname"
+            id="categoryname"
+            class="form-input"
+            placeholder="Category"
+          />
+        </div>
+        <div class="form-horizontal">
+          <label for="projectname" class="form-label"> Project </label>
+          <input
+            type="text"
+            name="projectname"
+            id="projectname"
+            class="form-input"
+            placeholder="Project"
+          />
+        </div>
 
-    <h3 class="title">Projects</h3>
-    <dl class="text" style="margin: 0 20px 20px 5px;">
-      <dt> See current projects </dt>
-      <dt> Add project </dt>
-    </dl>
+        <div style = "display: flex; justify-content:flex-end">
+          <button class="button-style">Add Task</button>
+        </div>
+      </form>
+    </div>
+    <h3 class="title">Start timer</h3>
+
+    <h3 class="title">Stop timer</h3>
+
   </div>
 </div>
 
@@ -53,6 +96,36 @@
 
   .general {
     font-family: var(--inline-title-font);
+
+  }
+
+  .form-label {
+    display: block;
+    margin-bottom: 5px;
+    margin-top: 10px;
+  }
+
+  .form-input {
+    width: 100%;
+  }
+
+  .input-flex {
+    display: flex;
+    gap: 20px;
+    margin-bottom: 15px;
+  }
+
+  .form-horizontal {
+    display:grid;
+    grid-template-columns: 30% 1fr;
+    grid-gap:5px;
+    margin-top: 5px;
+  }
+
+  .button-style {
+    padding: 10px 15px;
+    margin-top: 15px;
+    margin-bottom: 15px;
   }
 
 
