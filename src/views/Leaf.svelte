8c728724831ddecbc405f61ed375ca5092761644
Valeria Guidotti <valeriaguidotti@gmail.com>
2024-06-11 23:41:42 -0400

    feat: change form input width on side leaf

--- src/views/Leaf.svelte
+++ src/views/Leaf.svelte
@@ -9,62 +9,21 @@
   <h2 class="title">Task Tracker</h2>
   <hr class="divider">
 
-  <div style="margin-left:5px;">
+  <div style="margin-left:5px; margin-right:4px;">
     <div class="text" style="margin-bottom: 20px;">
-      <h2 class="title">Timer</h2>
+      <div class="section-title">
+        Timer
+      </div>
       <div class="button-horizontal">
         <button class="button-style"> Start Timer </button> 
         <button class="button-style"> Stop Timer </button> 
       </div>
-      <form action="new-manual-time" method="POST" class="form-horizontal">
-        <div>
-          <label for="taskname" class="form-label"> Task name </label>
-        </div>
-        <div>  
-          <input
-            type="text"
-            name="taskname"
-            id="taskname"
-            class="form-input"
-            placeholder="Task name"
-          />
-        </div>
-        <div>
-          <label for="categoryname" class="form-label"> Category </label>
-        </div>
-        <div>
-          <input
-            type="text"
-            name="categoryname"
-            id="categoryname"
-            class="form-input"
-            placeholder="Category"
-          />
-        </div>
-        <div>
-          <label for="projectname" class="form-label"> Project </label>
-        </div>
-        <div>
-          <input
-            type="text"
-            name="projectname"
-            id="projectname"
-            class="form-input"
-            placeholder="Project"
-          />
-        </div>
-      </form>
-
-    </div>
-
-    <div class="text" style="margin-top:20px;">
-      <h2 class="title" style="margin-bottom:20px;">Add manual time</h2>
       <form action="new-manual-time" method="POST">
         <div class="form-horizontal">
-          <div>
-            <label for="taskname" class="form-label"> Task name </label>
+          <div class="form-label">
+            <label for="taskname" > Task name </label>
           </div>
-          <div>  
+          <div class="form-input">  
             <input
               type="text"
               name="taskname"
@@ -73,32 +32,10 @@
               placeholder="Task name"
             />
           </div>
-          <div>
-            <label for="starttime" class="form-label">Start Time</label>
+          <div class="form-label">
+            <label for="categoryname" > Category </label>
           </div>
-          <div>
-            <input
-              type="datetime-local"
-              name="starttime"
-              class="form-input"
-              id="starttime"
-            />
-          </div>
-          <div>
-            <label for="endtime" class="form-label">End Time</label>
-          </div>
-          <div> 
-            <input
-              type="datetime-local"
-              name="endtime"
-              class="form-input"
-              id="endtime"
-            />
-          </div>
-          <div>
-            <label for="categoryname" class="form-label"> Category </label>
-          </div>
-          <div>
+          <div class="form-input">
             <input
               type="text"
               name="categoryname"
@@ -107,10 +44,87 @@
               placeholder="Category"
             />
           </div>
-          <div>
-            <label for="projectname" class="form-label"> Project </label>
+          <div class="form-label">
+            <label for="projectname" > Project </label>
           </div>
-          <div>
+          <div class="form-input">
+            <input
+              type="text"
+              name="projectname"
+              id="projectname"
+              class="form-input"
+              placeholder="Project"
+            />
+          </div>
+        </div>  
+      </form>
+
+    </div>
+
+    <div class="text" style="margin-top:20px;">
+      <div class="section-title">
+        Add manual time
+      </div>
+      <form action="new-manual-time" method="POST" style="margin-top:10px;">
+        <div class="form-horizontal">
+          <div class="form-label">
+            <label for="taskname" > Task name </label>
+          </div>
+          <div class="form-input">  
+            <input
+              type="text"
+              name="taskname"
+              id="taskname"
+              class="form-input"
+              placeholder="Task name"
+            />
+          </div>
+          <div class="form-label">
+            <label for="starttime" >Start Time</label>
+          </div>
+          <div class="form-input">
+            <input
+              type="text"
+              name="starttime"
+              class="form-input"
+              style="letter-spacing:-1px;"
+              id="starttime"
+              onfocus="(this.type='datetime-local')"
+              onblur="if(!this.value) this.type='text'"
+              placeholder="&#9716;"
+            />
+          </div>
+          <div class="form-label">
+            <label for="endtime" >End Time</label>
+          </div>
+          <div class="form-input"> 
+            <input
+              type="text"
+              name="endtime"
+              class="form-input"
+              style="letter-spacing:-1px;"
+              id="endtime"
+              onfocus="(this.type='datetime-local')"
+              onblur="if(!this.value) this.type='text'"
+              placeholder="&#9716;"
+            />
+          </div>
+          <div class="form-label">
+            <label for="categoryname" > Category </label>
+          </div>
+          <div class="form-input">
+            <input
+              type="text"
+              name="categoryname"
+              id="categoryname"
+              class="form-input"
+              placeholder="Category"
+            />
+          </div>
+          <div class="form-label">
+            <label for="projectname"> Project </label>
+          </div>
+          <div class="form-input">
             <input
               type="text"
               name="projectname"
@@ -145,37 +159,41 @@
   }
 
   .text {
-    font-size: var(--table-text-size);
+    font-size: var(--tab-font-size);
   }
 
   .general {
     font-family: var(--inline-title-font);
-
   }
 
+  .section-title {
+    font-size: var(--h4-size);
+    font-weight: var(--h3-weight);
+  }
+  
   .form-label {
     display: block;
-    font-size: var(--table-text-size);
-    margin-bottom: 5px;
+    font-size: var(--tab-font-size);
     margin-top: 10px;
+    margin-right: 2px;
   }
 
   .form-input {
     width: 100%;
-    font-size: var(--table-text-size);
+    font-size: var(--tab-font-size);
   }
 
   .form-horizontal {
-    display:grid;
+    display: grid;
     grid-template-columns: max-content 1fr;
-    grid-gap:5px;
+    grid-gap: 5px;
     margin-top: 5px;
   }
 
   .button-style {
     padding: 10px 15px;
-    margin-top: 15px;
-    margin-bottom: 15px;
+    margin-top: 10px;
+    margin-bottom: 10px;
   }
   
   .button-horizontal{
